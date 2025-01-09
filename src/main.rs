5a88dd73e1bf6f618a5c7a948de0effbb5970d44
DavidYannick <57354908+davidyannick86@users.noreply.github.com>
2025-01-09 15:48:13 +0100

    Refactor main function for improved error handling and clarity

--- src/main.rs
+++ src/main.rs
@@ -6,45 +6,52 @@ use flate2::write::GzEncoder;
 use flate2::Compression;
 use std::env::args;
 use std::fs::File;
-use std::io::copy;
-use std::io::BufReader;
-use std::time::Instant; // to show how long the compression took
+use std::io::{self, copy};
+use std::time::Instant;
 
-fn main() {
-    if args().len() != 3 {
-        eprintln!("Usage: <source> <destination>");
-        return;
+fn main() -> io::Result<()> {
+    // Check if the user provided the input and output file paths
+    if args().len() < 3 {
+        eprintln!(
+            "Usage: {} <input file> <output file>",
+            args().nth(0).unwrap()
+        );
+        std::process::exit(1);
     }
 
-    // Open the file to compress
-    let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
+    // Get the input and output file paths
+    let input_file_path = args().nth(1).expect("Missing input file");
+    let output_file_path = args().nth(2).expect("Missing output file");
+
+    // Open the input file
+    let mut input_file = File::open(&input_file_path)?;
 
     // Create the output file
-    let output = File::create(args().nth(2).unwrap()).unwrap();
+    let output_file = File::create(&output_file_path)?;
 
-    // Create the GzEncoder
-    let mut encoder = GzEncoder::new(output, Compression::default());
+    // Create a GzEncoder
+    let mut encoder = GzEncoder::new(output_file, Compression::best());
 
     // Start the timer
     let start = Instant::now();
 
     // Compress the file
-    copy(&mut input, &mut encoder).unwrap();
+    copy(&mut input_file, &mut encoder)?;
 
-    let output = encoder.finish().unwrap();
+    // Finish the compression
+    let output_file = encoder.finish()?;
 
     // Print the length of the source and destination files
-    println!(
-        "Source len: {:?}",
-        input.get_ref().metadata().unwrap().len()
-    );
+    println!("Source len: {:?}", input_file.metadata()?.len());
 
     // The destination file is the output file
-    println!("Destination len: {:?}", output.metadata().unwrap().len());
+    println!("Destination len: {:?}", output_file.metadata()?.len());
 
     // Print how long the compression took
     println!(
         "Compression took: {}",
         format!("{:?}", start.elapsed()).green()
     );
+
+    Ok(())
 }
